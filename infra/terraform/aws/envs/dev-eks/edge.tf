5da81956d078f2763522496e6e7d9526b3e3bfb1
smt <sunmingtao@gmail.com>
2026-04-25 12:11:49 +1000

    add provider

--- infra/terraform/aws/envs/dev-eks/edge.tf
+++ infra/terraform/aws/envs/dev-eks/edge.tf
@@ -1,3 +1,13 @@
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
 data "aws_acm_certificate" "www" {
   domain      = "smtdevboard.com"
   statuses    = ["ISSUED"]
