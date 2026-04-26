10c760b438527d871bb6b162fd93347670ff3851
Mingtao <sunmingtao@gmail.com>
2026-04-26 20:09:38 +1000

    Add devboard event consumer microservice

---
+++ apps/devboard-event-service/src/main/java/com/example/devboard/eventservice/listener/TaskEventListener.java
@@ -0,0 +1,15 @@
+package com.example.devboard.eventservice.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TaskEventListener {
+
+    @KafkaListener(topics = "${devboard.kafka.topics.task-events}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consumeTaskEvents(String eventPayload) {
+        log.info("Consumed task event from topic devboard.tasks: {}", eventPayload);
+    }
+}
